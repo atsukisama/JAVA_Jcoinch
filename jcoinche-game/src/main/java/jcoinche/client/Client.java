1b96a1fe9538c280976e1798a86a100aa32c6dbb
rusig_n <nicolas.rusig@epitech.eu>
2016-11-19 21:47:26 +0100

    debut

---
+++ jcoinche-game/src/main/java/jcoinche/client/Client.java
@@ -0,0 +1,21 @@
+package jcoinche.client;
+
+/**
+ * Created by rusig_n on 19/11/2016.
+ */
+
+public class Client {
+    private final String host;
+    private final int port;
+
+    public Client(String host, int port)
+    {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run()
+    {
+
+    }
+}
---
+++ jcoinche-game/src/main/java/jcoinche/client/Main.java
@@ -0,0 +1,10 @@
+package jcoinche.client;
+
+/**
+ * Created by rusig_n on 18/11/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+    }
+}
